317295fca6e940048c69ad9825fe741dbd91fade
Thalisson <thalissonca@gmail.com>
2018-06-19 14:13:53 -0300

    Caesar Cipher 1 Solution

---
+++ Problem Solving/Algorithms/Strings/caesar-cipher-1.c
@@ -0,0 +1,93 @@
+#include <assert.h>
+#include <limits.h>
+#include <math.h>
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+char* readline();
+
+// Complete the caesarCipher function below.
+
+// Please either make the string static or allocate on the heap. For example,
+// static char str[] = "hello world";
+// return str;
+//
+// OR
+//
+// char* str = "hello world";
+// return str;
+//
+char* caesarCipher(int n, char* s, int k) {
+    for(int i=0;i<n;i++){
+       if(s[i]>='a' && s[i]<='z'){
+           s[i]=(((s[i]-'a')+k)%26)+'a';
+           
+       }else if(s[i]>='A' && s[i]<='Z'){
+           s[i]=(((s[i]-'A')+k)%26)+'A';
+       }
+    }
+    return s;
+}
+
+int main()
+{
+    FILE* fptr = fopen(getenv("OUTPUT_PATH"), "w");
+
+    char* n_endptr;
+    char* n_str = readline();
+    int n = strtol(n_str, &n_endptr, 10);
+
+    if (n_endptr == n_str || *n_endptr != '\0') { exit(EXIT_FAILURE); }
+
+    char* s = readline();
+
+    char* k_endptr;
+    char* k_str = readline();
+    int k = strtol(k_str, &k_endptr, 10);
+
+    if (k_endptr == k_str || *k_endptr != '\0') { exit(EXIT_FAILURE); }
+
+    char* result = caesarCipher(n, s, k);
+
+    fprintf(fptr, "%s\n", result);
+
+    fclose(fptr);
+
+    return 0;
+}
+
+char* readline() {
+    size_t alloc_length = 1024;
+    size_t data_length = 0;
+    char* data = malloc(alloc_length);
+
+    while (true) {
+        char* cursor = data + data_length;
+        char* line = fgets(cursor, alloc_length - data_length, stdin);
+
+        if (!line) { break; }
+
+        data_length += strlen(cursor);
+
+        if (data_length < alloc_length - 1 || data[data_length - 1] == '\n') { break; }
+
+        size_t new_length = alloc_length << 1;
+        data = realloc(data, new_length);
+
+        if (!data) { break; }
+
+        alloc_length = new_length;
+    }
+
+    if (data[data_length - 1] == '\n') {
+        data[data_length - 1] = '\0';
+    }
+
+    data = realloc(data, data_length);
+
+    return data;
+}
